52e99396ce294bee45e829fc1774330e1dd1856d
Hyunseok Lim <prlhspt@gmail.com>
2020-08-14 07:16:33 +0900

    Expend Interfaces, Data Structure

--- src/main/java/com/rest/api/controller/v1/UserController.java
+++ src/main/java/com/rest/api/controller/v1/UserController.java
@@ -2,6 +2,9 @@ package com.rest.api.controller.v1;
 
 import com.rest.api.entity.User;
 import com.rest.api.entity.UserJpaRepo;
+import com.rest.api.model.response.ListResult;
+import com.rest.api.model.response.SingleResult;
+import com.rest.api.service.ResponseService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -17,21 +20,28 @@ import java.util.List;
 public class UserController {
 
     private final UserJpaRepo userJpaRepo;
+    private final ResponseService responseService;
 
-    @ApiOperation(value = "회원 조회", notes = "모든 회원을 조회한다")
+    @ApiOperation(value = "회원 리스트 조회", notes = "모든 회원을 조회한다")
     @GetMapping(value = "/user")
-    public List<User> findAllUser() {
-        return userJpaRepo.findAll();
+    public ListResult<User> findAllUser() {
+        return responseService.getListResult(userJpaRepo.findAll());
+    }
+
+    @ApiOperation(value  = "회원 단건 조회", notes = "userId로 회원을 조회한다")
+    @GetMapping(value = "/user/{msrl}")
+    public SingleResult<User> findUserById(@ApiParam(value = "회원ID", required = true) @PathVariable long msrl) {
+        return responseService.getSingleResult(userJpaRepo.findById(msrl).orElse(null));
     }
 
     @ApiOperation(value = "회원 입력", notes = "회원을 입력한다.")
     @PostMapping(value = "/user")
-    public User save(@ApiParam(value = "회원아이디", required = true) @RequestParam String uid,
+    public SingleResult<User> save(@ApiParam(value = "회원아이디", required = true) @RequestParam String uid,
                      @ApiParam(value = "회원이름", required = true) @RequestParam String name ) {
         User user = User.builder()
                 .uid("prlhspt@naver.com")
                 .name("임현석")
                 .build();
-        return userJpaRepo.save(user);
+        return responseService.getSingleResult(userJpaRepo.save(user));
     }
 }
---
+++ src/main/java/com/rest/api/service/ResponseService.java
@@ -0,0 +1,63 @@
+package com.rest.api.service;
+
+import com.rest.api.model.response.CommonResult;
+import com.rest.api.model.response.ListResult;
+import com.rest.api.model.response.SingleResult;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ResponseService {
+    public enum CommonResponse {
+        SUCESS(0, "성공하였습니다."),
+        FAIL(-1, "실패하였습니다");
+
+        @Getter
+        int code;
+
+        @Getter
+        String msg;
+
+        CommonResponse(int code, String msg) {
+            this.code = code;
+            this.msg = msg;
+        }
+    }
+
+    // 단일 처리
+    public <T> SingleResult<T> getSingleResult(T data) {
+        SingleResult<T> result = new SingleResult<>();
+        result.setData(data);
+        setSuccessResult(result);
+        return result;
+    }
+
+    // 다중 처리
+    public <T> ListResult<T> getListResult(List<T> list) {
+        ListResult<T> result = new ListResult<>();
+        result.setList(list);
+        setSuccessResult(result);
+        return result;
+    }
+
+    // 실패 처리
+    public CommonResult getFailResult() {
+        CommonResult result = new CommonResult();
+        result.setSucess(false);
+        result.setCode(CommonResponse.FAIL.getCode());
+        result.setMsg(CommonResponse.FAIL.getMsg());
+        return result;
+    }
+
+    // api 결과
+    private void setSuccessResult(CommonResult result) {
+        result.setSucess(true);
+        result.setCode(CommonResponse.SUCESS.getCode());
+        result.setMsg(CommonResponse.SUCESS.getMsg());
+    }
+
+}
