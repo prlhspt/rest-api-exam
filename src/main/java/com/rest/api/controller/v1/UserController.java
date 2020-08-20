ebd99c055a34152c18c989257db0887598718c51
prlhspt <prlhspt@naver.com>
2020-08-20 17:57:43 +0900

    api test\

--- src/main/java/com/rest/api/controller/v1/UserController.java
+++ src/main/java/com/rest/api/controller/v1/UserController.java
@@ -30,7 +30,7 @@ public class UserController {
     @ApiOperation(value  = "회원 단건 조회", notes = "userId로 회원을 조회한다")
     @GetMapping(value = "/user/{msrl}")
     public SingleResult<User> findUserById(@ApiParam(value = "회원ID", required = true) @PathVariable long msrl) {
-        return responseService.getSingleResult(userJpaRepo.findById(msrl).orElse(null));
+        return responseService.getSingleResult(userJpaRepo.findById(msrl).orElseThrow(Exception::new);
     }
 
     @ApiOperation(value = "회원 입력", notes = "회원을 입력한다.")
